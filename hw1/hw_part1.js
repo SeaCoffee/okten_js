60db86a86187cbbe965c4c73c73e065a9278310f
SeaCoffee <sapienti8sat@gmail.com>
2023-11-21 08:27:02 +0200

    Initial commit

---
+++ hw1/hw_part1.js
@@ -0,0 +1,59 @@
+variable1 = 'hello';
+console.log(variable1)
+
+variable2 = 'owu';
+console.log(variable2)
+
+variable3 = 'com';
+console.log(variable3)
+
+variable3 = 'ua';
+console.log(variable4)
+
+variable5 = 1;
+console.log(variable5)
+
+variable6 = 10;
+console.log(variable6)
+
+variable17 = -999;
+console.log(variable7)
+
+variable8 = 123;
+console.log(variable8)
+
+variable9 = 3.14;
+console.log(variable9)
+
+variable10 = 2.7;
+console.log(variable10)
+
+variable10 = 16;
+console.log(variable10)
+
+variable10 = true; /*Я так розумію, що це не строки мають бути?*/
+console.log(variable10)
+
+variable10 = false;
+console.log(variable10)
+
+
+firstName = "Lesia "
+
+middleName = 'Uriyivna '
+
+lastName='Khorishachenko'
+
+console.log(firstName + middleName + lastName)
+
+console.log(${firstName} ${middleName} ${lastName})
+
+
+let a = 100;
+console.log(typeof a, a)
+
+let b = '100';
+console.log(typeof b, b)
+
+let c = true;
+console.log(typeof c)
