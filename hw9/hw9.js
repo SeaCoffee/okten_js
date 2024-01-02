e401a759d00bda582081a8ea1b99d9a9c282f937
SeaCoffee <sapienti8sat@gmail.com>
2024-01-02 11:56:52 +0200

    hw12

--- hw9/hw9.js
+++ hw9/hw9.js
@@ -1,168 +1,47 @@
+let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Привет, мир!");
+    }, 2000);
+})
 
-/*- створити блок,
-    - додати йому класи wrap, collapse, alpha, beta
-- додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
-- додати цей блок в body.
-- клонувати його повністю, та додати клон в body.
+.then((resolve) => {
+     console.log(resolve)
+    }
+)
 
-let divediv = document.createElement("div")
+let uncertainPromise = new Promise((resolve, reject) => {
+    if (Math.random() > 0.5) {
+        resolve(10);
+    } else {
+        reject("Ошибка!");
+    }
+})
 
-divediv.classList.add('wrap', 'collapse', 'alpha', 'beta')
+.then((success)=>{console.log(success)})
+    .then((decline)=>{console.log(decline)})
 
-let styled = document.getElementsByClassName('wrap')
-
-
-for (let i = 0; i < styled.length; i++) {
-    styled[i].style.background = 'green'
-    styled[i].style.color = 'yellow'
-    styled[i].style.fontSize = '12px';
-}
-
-let clonedDiv = divediv.cloneNode(true)
-document.body.appendChild(clonedDiv)
-
-- Є масив:
-    ['Main','Products','About us','Contacts']
-Зробити ul в середині якої будуть лежати елементи масиву (кожен в своєму li)
-
-
-let arr = ['Main', 'Products', 'About us', 'Contacts'];
-
-let listUl = document.createElement('ul');
-
-for (let i = 0; i < arr.length; i++) {
-    let listLi = document.createElement('li');
-    listLi.textContent = arr[i];
-    listUl.appendChild(listLi);
-}
-
-document.body.appendChild(listUl);
-
-- Є масив
-let coursesAndDurationArray = [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
-];
-Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
-Завдання робити через цикли.
-
-for (let i = 0; i < coursesAndDurationArray.length; i++) {
-    let block = document.createElement('div')
-    // Объединение заголовка и продолжительности в одну строку
-    block.textContent = coursesAndDurationArray[i].title + coursesAndDurationArray[i].monthDuration
-
-    document.body.appendChild(block);
-}
-
-=========================
-
-    - Є масив
-let coursesAndDurationArray = [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
+let promises = [
+    Promise.resolve(1),
+    Promise.resolve(2),
+    Promise.resolve(3)
 ];
 
-
-За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
-    Завдання робити через цикли.
-
-    let coursesAndDurationArray = [
-    {title: 'JavaScript Complex', monthDuration: 5},
-    {title: 'Java Complex', monthDuration: 6},
-    {title: 'Python Complex', monthDuration: 6},
-    {title: 'QA Complex', monthDuration: 4},
-    {title: 'FullStack', monthDuration: 7},
-    {title: 'Frontend', monthDuration: 4}
-];
+Promise.all([promises]).then((all)=>{console.log(all)})
 
 
-for (let i = 0; i < courses.length; i++) {
-    let course = courses[i]
-    let block = document.createElement('div')
-    block.classList.add('item')
+let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Привет, мир!");
+    }, 2000);
+})
 
-    let header = document.createElement('h1')
-    header.classList.add('heading')
-
-    let paragraph = document.createElement('p')
-    paragraph.classList.add('description')
-    paragraph.textContent = course.monthDuration
-
-    block.appendChild(header)
-    block.appendChild(paragraph)
-    document.body.appendChild(block)
+    .then((resolve) => {
+            console.log(resolve)
+        }
+    )
+async function waitForHello() {
+    let message = await myPromise
+    console.log(message);
 }
 
-==========================
-
-- є масив simpsons, котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
-Проітерувати його, створиши для кожного об'єкту  масиву <div class='member'> та наповнити його данними з об'єкту.
-    Якщо людською мовою: під кожного члена родини зробити блок та наповнити його інформацією з цього об'єкту
-
-
-let simpsons = [
-    {
-        name: 'Bart',
-        surname: 'Simpson',
-        age: 10,
-        info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
-        photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
-    },
-    {
-        name: 'Homer',
-        surname: 'Simpson',
-        age: 40,
-        info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
-        photo: 'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
-    },
-    {
-        name: 'Marge',
-        surname: 'Simpson',
-        age: 38,
-        info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
-        photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
-    },
-    {
-        name: 'Lisa',
-        surname: 'Simpson',
-        age: 9,
-        info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
-        photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
-    },
-    {
-        name: 'Maggie',
-        surname: 'Simpson',
-        age: 1,
-        info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
-        photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
-    },
-];
-
-
-for (let i = 0; i < simpsons.length; i++) {
-    let simpsonBlock = document.createElement('div');
-    simpsonBlock.classList.add('member');
-    simpsonBlock.textContent = simpsons[i];
-
-    document.body.appendChild(simpsonBlock);
-}
-
-
-=========================
-    Цикл в циклі
-- Є масив coursesArray котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
-
-Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей,
- для властивості modules зробити список з елементами
-Приклад структири знаходиться у файлі example.png який лежить в папці з поточним фйлом
-*/
-
-
+waitForHello()
